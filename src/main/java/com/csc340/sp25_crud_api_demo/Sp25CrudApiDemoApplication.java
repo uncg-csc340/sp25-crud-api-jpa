d117dd0cad30a0453254dd261248c249be9654aa
uncg-csc340 <csc340sp24@gmail.com>
2025-02-19 19:58:07 -0500

    added project files

---
+++ src/main/java/com/csc340/sp25_crud_api_demo/Sp25CrudApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.csc340.sp25_crud_api_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp25CrudApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp25CrudApiDemoApplication.class, args);
+	}
+
+}
